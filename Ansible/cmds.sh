e1430fc3adf8987a7f9328ff7a1f1febfcfc46d7
ABD <iamabhitamboli@gmail.com>
2023-03-30 00:34:18 +0530

    new

---
+++ Ansible/cmds.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+export KUBECONFIG=/var/jenkins_home/workspace/Terraform-linode/Terraform/lke-cluster-config.yaml
+kubectl get node
+kubectl create secret generic regcred \
+--from-file=.dockerconfigjson=/var/jenkins_home/.docker/config.json \
+--type=kubernetes.io/dockerconfigjson
