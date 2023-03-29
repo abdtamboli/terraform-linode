b6879ab77bdfe343738665ef50bdd543dcf7a8d3
ABD <iamabhitamboli@gmail.com>
2023-03-30 00:52:25 +0530

    new

--- Ansible/cmds.sh
+++ Ansible/cmds.sh
@@ -6,3 +6,4 @@ kubectl get node
 kubectl create secret generic regcred \
 --from-file=.dockerconfigjson=/var/jenkins_home/.docker/config.json \
 --type=kubernetes.io/dockerconfigjson
+export K8S_AUTH_KUBECONFIG=/var/jenkins_home/workspace/Terraform-linode/Terraform/lke-cluster-config.yaml
